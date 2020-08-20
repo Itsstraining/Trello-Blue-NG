4647b5ed2e3e14cd0701d2168a783e72b3379469
dinhquoctuan <dinhquoctuan>
2020-08-20 15:58:42 +0700

    check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UiModule } from './modules/ui/ui.module';
-<<<<<<< HEAD
-import { NavbarComponent } from './navbar/navbar/navbar.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatCardModule} from '@angular/material/card';
-import { LoginComponent } from './login/login/login.component';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { AuthService } from './service/auth.service';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireStorageModule } from '@angular/fire/storage';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { environment } from 'src/environments/environment';
-import {MatInputModule} from '@angular/material/input'
-import { RegistrationComponent } from './registration/registration/registration.component';
-
-
-
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    LoginComponent,
-    RegistrationComponent
-=======
 import { Navbar1Component } from './navbar1/navbar1.component';
 import { HomeComponent } from './home/home.component';
 import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatSliderModule } from '@angular/material/slider';
+import { AngularFireModule } from '@angular/fire';
+import { MatCardModule } from '@angular/material/card';
+import { environment } from 'src/environments/environment';
+import { AuthService } from './service/auth.service';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
     Navbar1Component,
     HomeComponent,
-    SidebarComponent
->>>>>>> 82c69b91c14e36cd22238508e75e9a9d30608b4f
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
